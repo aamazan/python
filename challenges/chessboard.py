85f727ee70718c49e2eb27438e39343aa1d60c04
aamazan <armaan.amazan@gmail.com>
2021-06-29 15:18:32 -0700

    BUG: check_board is outputting all 0s for some reason

--- challenges/chessboard.py
+++ challenges/chessboard.py
@@ -7,7 +7,7 @@
 # input: 64-bit string, integer
 # output: 64-bit string
 def flip(s, flipnum):
-    print(s)
+    print("flip input: " + s+"\n")
     if s[flipnum] == "0":
         temp = list(s)
         temp[flipnum] = "1"
@@ -16,7 +16,7 @@ def flip(s, flipnum):
         temp = list(s)
         temp[flipnum] = "0"
         s = "".join(temp)
-    print(s)
+    print("flip output: " +s+"\n")
     return s
 
 
@@ -28,7 +28,7 @@ def check_board(s):
     check = 0 # for determining number of heads per section
     resbit = 5 # current bit of result being checked for
     # columns: designated by %8 (e.g. 8%8 == 0%8 = 0, so same column)
-
+    print("cb input: " + s + "\n")
     # every other  (1,3,5,7) ((0-7))
     for i in range(0,64):
         if i%8 == 1 or i%8 == 3 or i%8 == 5 or i%8 == 7:
@@ -112,9 +112,10 @@ def check_board(s):
     temp = list(result)
     temp[resbit] = checkres
     result = "".join(temp)
-
+    print("cb output: " + result + "\n")
 
     return result
+
 
 # "Function prisoner1 takes two inputs: a series S of 64 bits,
 # and a number X from 0 to 63 (inclusive).
@@ -124,6 +125,7 @@ def check_board(s):
 def prisoner1(s,x):
     # count heads in rows, columns to determine current encoding
     # interpreting "magic square" as opposite value
+    print("p1 input: " + s + ", " + str(x)+"\n")
     copy = flip(s,x) # make copy with flipped x'th bit
     bits = '{0:06b}'.format(x) # get binary for x, outputs bitstring
     board = check_board(copy) # board = current state bitstr
@@ -136,7 +138,8 @@ def prisoner1(s,x):
     	else:
     		result = result + "1"
     result = int(result,2) # convert bitstr to int
-    return result
+    print("p1 output: " + str(result)+"\n")
+    return result #square to be flipped
 
 
 # "Function prisoner2 takes one input: a series T of 64 bits.
@@ -144,13 +147,15 @@ def prisoner1(s,x):
 # input: 64-bit string
 # output: integer
 def prisoner2(t):
-    print(t)
-    print(check_board(t)) # bitstr to decode
-
+    #print(t)
+    print("p2 input: " + t)
+    bs = check_board(t) # bitstr to decode
+    print("p2 output: " + str(bs) +"\n")
 
     #bits = int(te,2) # convert bitstr to int
     #print(bits)
     return
+
 
 # Function solve provided with the prompt
 def solve(S, X):
@@ -159,10 +164,9 @@ def solve(S, X):
     return prisoner2(T) == X
 
 
-
-test = "11111000000110000110010101010010010010011111100111110101100000100111111110101010"
-print("check_board: " + check_board(test))
+test = "1111100000011000011001010101001001001001111110011111010110000010"
+#print("check_board: " + check_board(test))
 # print("p1: " + str(prisoner1(test,5)))
-
+#prisoner2(test)
 prisoner2(flip(test, prisoner1(test, 5)))
 # print(solve(test,5))
